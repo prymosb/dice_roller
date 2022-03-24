3f55b0bf17e1fdd44f28f9440a0c83789cd2d12c
Boris Prymost <borisprymost@gmail.com>
2022-03-23 20:39:24 -0700

    adding argument description

--- roll.py
+++ roll.py
@@ -4,14 +4,15 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('roll', type=str)
+    parser.add_argument(
+        'dice', type=str, help='number and type of dice you want to roll with or without a modifier. ex: 2d20+4')
 
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    print(f'ARG: {args.roll}')
+    print(f'ARG: {args.dice}')
 
 
 if __name__ == "__main__":
